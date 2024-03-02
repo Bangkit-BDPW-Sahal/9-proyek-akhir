7a33db00c58d1a20be1ad547af317c3a707713dd
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2024-03-02 16:28:51 +0700

    add changes

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 /** @format */
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropDownMenu = document.querySelector(".dropdown_menu");
 
-function toggleNav() {
-  var navList = document.querySelector(".nav-list");
-  var burgerMenu = document.querySelector(".burger-menu");
+toggleBtn.onclick = function () {
+  dropDownMenu.classList.toggle("open");
+  const isOpen = dropDownMenu.classList.contains("open");
+  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+};
 
-  navList.classList.toggle("active");
-  burgerMenu.classList.toggle("active");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggleContent");
+  const hiddenContent = document.getElementById("hiddenContent");
+
+  toggleButton.addEventListener("click", function () {
+    hiddenContent.classList.toggle("hidden");
+  });
+});
